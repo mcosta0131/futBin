8887d59bdac06d58fa75503a66c9521662a1212d
MATEUS COSTA DOS SANTOS <mcosta0131@gmail.com>
2019-11-14 09:35:51 -0300

    Alex telles ID

--- src/main/java/com/br/service/CheckPriceSeleniumService.java
+++ src/main/java/com/br/service/CheckPriceSeleniumService.java
@@ -2,18 +2,13 @@ package com.br.service;
 
 import java.util.Date;
 import java.util.Properties;
-
-import javax.mail.Address;
-import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
-import javax.mail.Store;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,7 +16,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.br.enumerator.PlayersEnum;
 import com.br.model.mongo.Players;
 import com.br.repository.PlayersRepository;
 
@@ -39,10 +33,11 @@ public class CheckPriceSeleniumService {
 		ChromeOptions chromeOptions = new ChromeOptions();
 		chromeOptions.setHeadless(true);
 		WebDriver driver = new ChromeDriver(chromeOptions);
-		driver.get("https://www.futbin.com/20/player/466");
+		driver.get("https://www.futbin.com/20/player/43729/alex-telles");
 		String price = driver.findElement(By.id("ps-lowest-1")).getText();
 		String precoTratado = price.replace(",", "");
 		fillEntity(Long.parseLong(precoTratado));
+		driver.close();
 		driver.quit();
 
 	}
@@ -50,15 +45,15 @@ public class CheckPriceSeleniumService {
 	public void fillEntity(Long preco) {
 
 		Players jogador = new Players();
-		jogador.setNome("Mané");
+		jogador.setNome("Alex telles IF");
 		jogador.setPreco(preco);
 		jogador.setDataAtualizacaoPreco(new Date());
 		rep.insert(jogador);
 
-		if(preco <= 350000) {
+		if(preco >= 120000) {
 			sendEmail(preco);
 		}else{
-			System.out.println("JOGADOR NÃO ESTA A BAIXO DO PREÇO BASE");
+			System.out.println("JOGADOR NÃO ESTA A CIMA DO PREÇO BASE");
 		}
 
 	}
